b81ace6237210e60a72075dc5749f4837ea11706
chlorochrule <minami.polly@gmail.com>
2018-09-16 01:49:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='',
+    name='ssmethod',
     version='0.1.0',
     description='',
     long_description=readme,
     author='Naoto MINAMI',
     author_email='minami.polly@gmail.com',
-    install_requires=[''],
-    url='',
+    install_requires=['numpy', 'scipy', 'sklearn'],
+    url='https://github.com/chlorochrule/ssmethod',
     license=license,
     packages=find_packages(exclude=('tests', 'examples'))
 )
